d321d11937482d183f69876cf30aafdce3f1419d
Arthur Corona <coronaggp@gmail.com>
2022-09-22 18:22:45 -0300

    invalid messages password/email

--- script/login.js
+++ script/login.js
@@ -1,9 +1,11 @@
-function validateFields() {
-    const emailValid = isEmailValid()
-    document.getElementById('recover-password').disabled = !emailValid
+function onChangeEmail() {
+    toggleButtonsDisable()
+    toggleEmailErrors()
+}
 
-    const passwordValid = isPasswordValid()
-    document.getElementById('login').disabled = !emailValid || !passwordValid
+function onChangePassword() {
+    toggleButtonsDisable()
+    togglePasswordErrors()
 }
 
 function isEmailValid() {
@@ -12,6 +14,40 @@ function isEmailValid() {
         return false
     } 
     return validateEmail(email)
+}
+
+function toggleEmailErrors() {
+    const email = document.getElementById('email').value
+    if (!email) {
+        document.getElementById('email-error').style.display = 'block'
+    } else {
+        document.getElementById('email-error').style.display = 'none'
+    }
+
+    if(validateEmail(email)) {
+        document.getElementById('email-invalid').style.display = 'none'
+    } else {    
+        document.getElementById('email-invalid').style.display = 'block'
+    }
+
+}
+
+function togglePasswordErrors() {
+    const password = document.getElementById('password').value
+    if (!password) {
+        document.getElementById('password-error').style.display = 'block'
+    } else {
+        document.getElementById('password-error').style.display = 'none'
+    }
+}
+
+function toggleButtonsDisable () {
+    
+    const emailValid = isEmailValid()
+    document.getElementById('recover-password').disabled = !emailValid
+
+    const passwordValid = isPasswordValid()
+    document.getElementById('login').disabled = !emailValid || !passwordValid
 }
 
 function isPasswordValid() {
