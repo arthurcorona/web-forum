18f330864bdbd83aa67a1fd49f6eadbb537b9893
Arthur Corona <95508790+arthurcorona@users.noreply.github.com>
2022-09-28 20:48:53 -0300

    alerta de erro de login adicionado

--- script/login.js
+++ script/login.js
@@ -1,3 +1,21 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-2Dy12VaK64ccH8vRh6hq8LUd3NQy6Q8",
+    authDomain: "sinion.firebaseapp.com",
+    projectId: "sinion",
+    storageBucket: "sinion.appspot.com",
+    messagingSenderId: "1032781603491",
+    appId: "1:1032781603491:web:a7049ecfa3531dca7cd516",
+    measurementId: "G-QYV90PXCG5"
+  };
+
+    firebase.initializeApp(firebaseConfig)
+
+    const database = firebase.firestore() // banco de dados fireabse
+    const auth = firebase.auth() //authenticator firebase
+    const storage = firebase.storage() 
+    const ref = storage.ref("/folder do firebase") // referencia para a pasta raiz do storage no firebase, da pra especificar ainda mais o arquivo preenchendo o ()
+
+
 function onChangeEmail() {
     toggleButtonsDisable()
     toggleEmailErrors()
@@ -9,9 +27,22 @@ function onChangePassword() {
 }
 
 function loginSinion() {
-    window.location.href = "../public/index.html"
+    firebase.auth().signInWithEmailAndPassword(form.email().value, 
+        form.password().value)
+            .then(response => {
+                window.location.href = "../public/index.html" 
+        }).catch(error => {
+            alert(getErroeMessage(error))
+    })
 }
 
+function getErroeMessage(error) {
+    if (error.code =="auth/user-not-found") {
+        return "Usuário não encontrádo"
+    }
+    return error.message
+}
+ 
 function registerSinion() {
     window.location.href = "../html/register.html"
 }
