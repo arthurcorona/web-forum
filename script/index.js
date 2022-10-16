037a57a295d20ea3d2b656e95cc08dff7cb16b05
arthurcorona <coronaggp@gmail.com>
2022-10-16 17:46:01 -0300

    options account

--- script/index.js
+++ script/index.js
@@ -1,3 +1,13 @@
+function showMenuAccount() {
+    let buttons = document.getElementById("options_account")
+    
+    if (buttons.style.display === "block") {
+      buttons.style.display = "none";
+    } else {
+      buttons.style.display = "block";
+    }
+}
+ 
 function signOut() {
     firebase.auth().signOut().then(() => {
         window.location.href = "../html/login.html"
