1070a3f86ec0551d825ea9fab4647341c0970b82
arthurcorona <coronaggp@gmail.com>
2022-10-19 23:20:20 -0300

    little rework in thread.js

---
+++ script/thread.js
@@ -0,0 +1,45 @@
+let threads = [
+    {
+        id: 1,
+        title: "A engenharia em cima",
+        author: "Autor",
+        date: Date.now(),
+        content: "conteúdo",
+        comments: [
+            {
+                author: "",
+                date: Date.now(),
+                content: "conteúdo do comentário do comentário"
+
+            },
+            {
+                author: "",
+                date: Date.now(),
+                content: "conteúdo do comentário do comentário em outro comentário"
+
+            }
+
+        ]
+    },
+    {
+        id: 2,
+        title: "A engenharia bélica",
+        author: "Autor",
+        date: Date.now(),
+        content: "conteúdo",
+        comments: [
+            {
+                author: "",
+                date: Date.now(),
+                content: "conteúdo do comentário do comentário"
+            },
+            {
+                author: "",
+                date: Date.now(),
+                content: "conteúdo do comentário do comentário em outro comentário"
+            }
+        ]
+    }
+]
+
+let thread
