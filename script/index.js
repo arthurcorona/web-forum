493057dd6cb97b0128df82c0a0f0d75c8ca9c69e
arthurcorona <coronaggp@gmail.com>
2022-10-20 22:10:56 -0300

    moving to index.js

--- script/index.js
+++ script/index.js
@@ -21,3 +21,80 @@ console.log("");
 auth.onAuthStateChanged(user => {
     console.log(user.uid);
 })
+
+// create threads
+
+let threads = [
+    {
+        id: 1,
+        title: "A engenharia em cima",
+        author: "Autor",
+        date: Date.now(),
+        content: "conteúdo",
+        comments: [
+            {
+                author: "",
+                date: Date.now(),
+                content: "conteúdo do comentário do comentário"
+
+            },
+            {
+                author: "",
+                date: Date.now(),
+                content: "conteúdo do comentário do comentário em outro comentário"
+
+            }
+
+        ]
+    },
+    {
+        id: 2,
+        title: "A engenharia bélica",
+        author: "Autor",
+        date: Date.now(),
+        content: "conteúdo",
+        comments: [
+            {
+                author: "",
+                date: Date.now(),
+                content: "conteúdo do comentário do comentário"
+            },
+            {
+                author: "",
+                date: Date.now(),
+                content: "conteúdo do comentário do comentário em outro comentário"
+            }
+        ]
+    }
+]
+
+
+console.log(threads);
+
+
+
+let container = document.querySelector('ol')
+
+for (let thread of threads) {
+    let html = `
+                <li class="row">
+                <a class="to-threads" href="../html/thread.html?${thread.id}">
+                    <h4 class="post-title">
+                        ${thread.title}
+                    </h4>
+                    <div class="post-content">
+                        <p class="timestamp paragraphs-row">
+                            ${new Date(thread.date).toLocaleString()}
+                        </p>
+                        <p class="comment-count paragraphs-row">
+                            ${thread.comments.length} comments
+                        </p>
+                    </div>
+                </a>
+                </li>
+    `
+
+    container.insertAdjacentHTML('beforeend', html)
+
+}
+
